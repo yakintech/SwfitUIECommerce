42205f0e5141a00b631024fcde7b1d80815a8474
yakintech <cagatayyildiz87@gmail.com>
2023-10-08 10:40:57 +0300

    Initial Commit

---
+++ SwiftUIECommerceSample/SwiftUIECommerceSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIECommerceSampleApp.swift
+//  SwiftUIECommerceSample
+//
+//  Created by Çağatay Yıldız on 8.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIECommerceSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
